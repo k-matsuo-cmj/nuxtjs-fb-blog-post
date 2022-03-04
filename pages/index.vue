88bf6aaa67cc8e0d670b6a1f06d0dc9943f409ce
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-04 18:49:14 +0900

    Post page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>Index</div>
+  <v-container>
+    <v-row>
+      <v-col v-if="!blogs.length" cols="12">
+        <span>No Blogs...</span>
+      </v-col>
+      <v-fab-transition>
+        <v-btn
+          color="deep-orange darken-2"
+          dark
+          absolute
+          large
+          bottom
+          right
+          fab
+          to="/post"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      blogs: []
+    }
+  }
 }
 </script>
---
+++ pages/post.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-toolbar dense dark color="secondary">
+            <v-toolbar-title>New Blog</v-toolbar-title>
+            <v-spacer />
+            <v-btn icon to="/">
+              <v-icon>mdi-close</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Title" />
+              <v-textarea rows="10" label="Content" />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn large color="secondary" to="/">
+              post
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'PostPage'
+}
+</script>
+
+<style>
+
+</style>
