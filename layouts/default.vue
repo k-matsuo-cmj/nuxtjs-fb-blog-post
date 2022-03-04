b59fc3c5a608c3997178ea3b21bf54a7c7b59086
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-04 17:09:47 +0900

    Login Page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
-    <Nuxt />
+    <v-app-bar app dark color="brown darken-1">
+      <v-toolbar-title>Blog Post</v-toolbar-title>
+      <v-spacer />
+      <v-btn icon :to="'/login'">
+        <v-icon>
+          mdi-account-circle
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app dark color="brown darken-1" padless>
+      <v-col class="text-center text-caption">
+        <span>&copy; CyberMissions {{ new Date().getFullYear() }}</span>
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
---
+++ pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="12" sm="10">
+      <v-card>
+        <v-toolbar dense dark color="primary">
+          <v-toolbar-title>Login</v-toolbar-title>
+          <v-spacer />
+          <v-btn icon to="/">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Email" />
+            <v-text-field label="Password" />
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <nuxt-link to="/">
+            Sign up
+          </nuxt-link>
+          <v-spacer />
+          <v-btn large color="primary" to="/">
+            Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage'
+}
+</script>
+
+<style>
+
+</style>
