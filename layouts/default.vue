53d64df31b0d788d5c7c1582ddf865042be535d7
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-10 18:33:31 +0900

    logout menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app dark color="brown darken-1">
+    <v-app-bar app :color="baseColor.color" :dark="baseColor.dark">
       <v-toolbar-title>Blog Post</v-toolbar-title>
       <v-spacer />
       <v-btn v-if="!$store.getters.isAuthenticated" icon :to="'/login'">
@@ -8,19 +8,29 @@
           mdi-account-circle
         </v-icon>
       </v-btn>
-      <v-btn v-else icon @click="logout">
-        {{ $store.state.user.displayName }}
-        <v-icon>
-          mdi-dots-vertical
-        </v-icon>
-      </v-btn>
+      <v-menu v-else bottom transition="slide-y-transition">
+        <template #activator="{ on }">
+          <v-btn text v-on="on">
+            {{ $store.state.user.displayName }}
+            <v-icon>
+              mdi-dots-vertical
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-list :color="baseColor.color" :dark="baseColor.dark">
+          <v-list-item :key="'logout'" @click="logout">
+            <v-icon>mdi-exit-to-app</v-icon>
+            Logout
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer app dark color="brown darken-1" padless>
+    <v-footer app :color="baseColor.color" :dark="baseColor.dark" padless>
       <v-col class="text-center text-caption">
         <span>&copy; CyberMissions {{ new Date().getFullYear() }}</span>
       </v-col>
@@ -33,6 +43,14 @@ export default {
   name: 'DefaultLayout',
   data () {
     return {
+    }
+  },
+  computed: {
+    baseColor () {
+      return {
+        color: this.$store.getters.isAuthenticated ? 'brown lighten-3' : 'brown darken-1',
+        dark: !this.$store.getters.isAuthenticated
+      }
     }
   },
   methods: {
