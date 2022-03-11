82df7e5ed7ee468f04abdbb9e732159c5454d927
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-11 16:28:24 +0900

    secure signup password

--- pages/signup.vue
+++ pages/signup.vue
@@ -12,8 +12,22 @@
         <v-card-text>
           <v-form ref="form" lazy-validation>
             <v-text-field v-model="email" type="email" label="E-mail" :rules="rules.email" />
-            <v-text-field v-model="password" type="password" label="Password" :rules="rules.password" />
-            <v-text-field v-model="confirmPassword" type="password" label="Confirm Password" :rules="rules.confirmPassword" />
+            <v-text-field
+              v-model="password"
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              :rules="rules.password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+            <v-text-field
+              v-model="confirmPassword"
+              :type="showConfirmPassword ? 'text' : 'password'"
+              label="Confirm Password"
+              :rules="rules.confirmPassword"
+              :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showConfirmPassword = !showConfirmPassword"
+            />
             <v-text-field v-model="userName" type="text" label="Your Name" :rules="rules.name" />
           </v-form>
         </v-card-text>
@@ -47,7 +61,9 @@ export default {
         ],
         password: [
           v => !!v || 'Password is required',
-          v => (v && v.length >= 8) || 'Password must be more than 8 characters'
+          v => (v && v.length >= 8) || 'Password must be more than 8 characters',
+          v => (v && v.match(/^((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]))([a-zA-Z0-9])+$/)) ||
+            'Password must contain at least 1 upper case letter, 1 lower case letter, and 1 numeric character.'
         ],
         confirmPassword: [
           v => (v === this.password) || 'Passwords does not match'
@@ -56,7 +72,9 @@ export default {
           v => !!v || 'Name is required'
         ]
       },
-      message: ''
+      message: '',
+      showPassword: false,
+      showConfirmPassword: false
     }
   },
   methods: {
